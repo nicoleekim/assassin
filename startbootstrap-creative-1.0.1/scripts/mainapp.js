1c088f45ee35933b80096b01070fde7f639a9b26
nicolekim <Nicole@Nicoles-MacBook-Pro.local>
2015-10-03 17:31:52 -0500

    halp

---
+++ startbootstrap-creative-1.0.1/scripts/mainapp.js
@@ -0,0 +1,39 @@
+var mainApp = angular.module("mainApp", []);
+mainApp.controller("MainCtrl", function($scope) {
+    $scope.assassins = [
+        {name: "Joe Biden", src:"../images/portraits/biden.jpg", target:""},
+        {name: "Jeb Bush", src:"../images/portraits/jeb.jpg", target:""},
+        {name: "Ben Carson", src:"../images/portraits/carson.jpg", target:""},
+        {name: "Chris Christie", src:"../images/portraits/christie.jpg", target:""},
+        {name: "Hillary Clinton", src:"../images/portraits/hillary.jpg", target:""},
+        {name: "Ted Cruz", src:"../images/portraits/cruz.jpg", target:""},
+        {name: "Carly Fiorina", src:"../images/portraits/fiorina.jpg", target:""},
+        {name: "Mike Huckabee", src:"../images/portraits/huckabee.jpg", target:""},
+        {name: "John Kasich", src:"../images/portraits/kasich.JPG"},
+        {name: "Martin O'Malley", src:"../images/portraits/omalley.jpg", target:""},
+        {name: "Rand Paul", src:"../images/portraits/paul.jpg", target:""},
+        {name: "Marco Rubio", src:"../images/portraits/rubio.jpg", target:""},
+        {name: "Bernie Sanders", src:"../images/portraits/bernie.jpg", target:""},
+        {name: "Rick Santorum", src:"../images/portraits/santorum.jpg", target:""},
+        {name: "Donald Trump", src:"../images/portraits/trump.jpg", target:""},
+        {name: "Scott Walker", src:"../images/portraits/walker.jpg", target:""}
+    ];
+
+    // Randomly assigns targets to each of the assassins
+    $scope.assignTargets = function() {
+        var init = [];
+        for (var i = 0; i < $scope.assassins.length; i++) {
+            init.push($scope.assassins[i].name);
+        }
+        for (var i = 0; i < init.length-1; i++) {
+            var switchWith = Math.floor(Math.random() * (init.length - (i+1)) + (i+1));
+            var temp = init[switchWith];
+            init[switchWith] = init[i];
+            init[i] = temp;
+        }
+        for (var i = 0; i < init.length; i++) {
+            $scope.assassins[i].target = init[i].name;
+            console.log($scope.assassins[i].name + " -> " + init[i]);
+        }
+    };
+});
